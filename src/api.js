28844efd4e10dc004f62e3e3106670ba59b7ce59
GaneshKantle <ganeskantleh@gmail.com>
2025-07-27 23:14:32 +0530

    Task3 completed!

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://taskflow-backend-op7s.onrender.com/api";
 
 export const registerUser = async (user) => {
   const response = await fetch(`${API_URL}/register`, {
